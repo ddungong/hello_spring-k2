82c0e41242be71edb8aa908b35ddee2db40920aa
ddungong <kmg000719@naver.com>
2023-09-27 18:41:06 +0900

    commit

---
+++ src/main/java/hello/hello_spring/k2/HelloSpringK2Application.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.k2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringK2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringK2Application.class, args);
+	}
+
+}
